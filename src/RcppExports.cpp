58334e66cbea7f601191548b0f6ae9829e4d8118
tom-b <tom.bodenheimer@gmail.com>
2020-04-08 22:05:53 +0000

    Update for removing duplicate edges.
    A new C++ file was added with a function
    dedup_links(links)
    
    that will remove duplicate edges and edges
    of forms:
    
    [i j wt]
    [j i wt]
    
    Also, if i,j, or wt = 0, that edge is
    filtered out.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,9 +5,20 @@
 
 using namespace Rcpp;
 
+// dedup_links
+NumericMatrix dedup_links(NumericMatrix links);
+RcppExport SEXP _rgrappolo_dedup_links(SEXP linksSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< NumericMatrix >::type links(linksSEXP);
+    rcpp_result_gen = Rcpp::wrap(dedup_links(links));
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_parallel_jce
 NumericMatrix rcpp_parallel_jce(NumericMatrix mat);
-RcppExport SEXP _FastPG_rcpp_parallel_jce(SEXP matSEXP) {
+RcppExport SEXP _rgrappolo_rcpp_parallel_jce(SEXP matSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -18,7 +29,7 @@ END_RCPP
 }
 // parallel_louvain
 Rcpp::List parallel_louvain(NumericMatrix links);
-RcppExport SEXP _FastPG_parallel_louvain(SEXP linksSEXP) {
+RcppExport SEXP _rgrappolo_parallel_louvain(SEXP linksSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
@@ -29,12 +40,13 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_FastPG_rcpp_parallel_jce", (DL_FUNC) &_FastPG_rcpp_parallel_jce, 1},
-    {"_FastPG_parallel_louvain", (DL_FUNC) &_FastPG_parallel_louvain, 1},
+    {"_rgrappolo_dedup_links", (DL_FUNC) &_rgrappolo_dedup_links, 1},
+    {"_rgrappolo_rcpp_parallel_jce", (DL_FUNC) &_rgrappolo_rcpp_parallel_jce, 1},
+    {"_rgrappolo_parallel_louvain", (DL_FUNC) &_rgrappolo_parallel_louvain, 1},
     {NULL, NULL, 0}
 };
 
-RcppExport void R_init_FastPG(DllInfo *dll) {
+RcppExport void R_init_rgrappolo(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
---
+++ src/dedup_links.cpp
@@ -0,0 +1,70 @@
+// [[Rcpp::depends(BH)]]
+#include <Rcpp.h>
+#include <boost/container_hash/hash.hpp>
+using namespace Rcpp;
+
+struct Edge {
+  std::tuple<long,long,double> edge;
+  
+  bool operator==(const Edge& e) const{
+    long v1 = std::get<0>(this->edge);
+    long v2 = std::get<1>(this->edge);
+    long e_v1 = std::get<0>(e.edge);
+    long e_v2 = std::get<1>(e.edge);
+    return ((v1==e_v1)&&(v2==e_v2))
+           ||
+           ((v1==e_v2)&&(v2==e_v1));
+  }
+};
+
+class edgeHashFn{
+public:
+  std::size_t operator()(const Edge& e) const{
+    std::size_t seed = 0;
+    if(std::get<0>(e.edge) < std::get<1>(e.edge)){
+      boost::hash_combine(seed, std::get<0>(e.edge));
+      boost::hash_combine(seed, std::get<1>(e.edge));
+    }
+    else{      
+      boost::hash_combine(seed, std::get<1>(e.edge));
+      boost::hash_combine(seed, std::get<0>(e.edge));
+    }
+    return seed;
+  }
+};
+
+class edgeCompareFn{
+public:
+  std::size_t operator()(const Edge& e1,const Edge& e2) const{
+    return (std::get<0>(e1.edge)==std::get<0>(e2.edge)
+              &&
+            std::get<1>(e1.edge)==std::get<1>(e2.edge))
+           ||
+           (std::get<0>(e1.edge)==std::get<1>(e2.edge)
+              &&
+            std::get<1>(e1.edge)==std::get<0>(e2.edge));
+  }
+};
+
+// [[Rcpp::export]]
+NumericMatrix dedup_links(NumericMatrix links){
+  std::unordered_set<Edge,edgeHashFn,edgeCompareFn> edgeSet;
+
+  for(long i = 0; i<links.nrow();i++){
+    if(links(i,0)!=0 && links(i,1)!=0 && links(i,2)!=0.0){
+      Edge e = {std::make_tuple(links(i,0),links(i,1),links(i,2))};
+      edgeSet.insert(e);
+    }
+  }
+
+  NumericMatrix deduplicated_edges(edgeSet.size(),3);
+  
+  long row_index=0;
+  for(Edge e:edgeSet){
+    deduplicated_edges(row_index,0)=std::get<0>(e.edge);
+    deduplicated_edges(row_index,1)=std::get<1>(e.edge);
+    deduplicated_edges(row_index,2)=std::get<2>(e.edge);
+    row_index++;
+  }
+  return deduplicated_edges;
+}
