f49ad91082be44e3db40e51fd8111ec7a3eee80e
Stuart R. Jefferys <srj@unc.edu>
2021-01-20 02:51:11 +0000

    Dev: Docker build draft outline
    Docker build code is not finished - need dev updated to build.

--- Docker/build.sh
+++ Docker/build.sh
@@ -7,18 +7,18 @@ imageCreated="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 context="."
 name="fastpg"
 baseVersion="3.10"  # The bioconductor version
-toolVersion="0.0.6" # This R package version
+toolVersion="0.0.7" # This R package version
 domain="jefferys"   # GitHub account
 
 TAG1="${domain}/${name}:${baseVersion}_${toolVersion}"
-TAG2="${domain}/${name}:${baseVersion}_latest"
-TAG3="${domain}/${name}:latest"
+# TAG2="${domain}/${name}:${baseVersion}_latest"
+# TAG3="${domain}/${name}:latest"
 
 docker build \
              --build-arg toolVersion="${toolVersion}" \
              --build-arg imageCreated="${imageCreated}" \
              -t "$TAG1" \
-             -t "$TAG2" \
-             -t "$TAG3" \
+#             -t "$TAG2" \
+#             -t "$TAG3" \
              $@ \
              "${context}"
